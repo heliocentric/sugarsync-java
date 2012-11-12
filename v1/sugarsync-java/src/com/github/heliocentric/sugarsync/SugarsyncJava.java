72b6950f28260ef85d69fd56c473ec5529102cff
Dylan Cochran <heliocentric@gmail.com>
2012-11-12 18:51:42 -0500

    * Add sqlite4java-282.

--- v1/sugarsync-java/src/com/github/heliocentric/sugarsync/SugarsyncJava.java
+++ v1/sugarsync-java/src/com/github/heliocentric/sugarsync/SugarsyncJava.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package sugarsync.java;
+package com.github.heliocentric.sugarsync;
 
 /**
  *
@@ -13,7 +13,8 @@ public class SugarsyncJava {
     /**
      * @param args the command line arguments
      */
+    public SugarSyncAPI Connection;
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Test");
     }
 }
