1e92a3ac07d20c9728349ff40e15ee3f465504cd
Dylan Cochran <heliocentric@gmail.com>
2012-11-12 18:24:19 -0500

    * Add project files.

---
+++ v1/sugarsync-java/src/sugarsync/java/SugarsyncJava.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sugarsync.java;
+
+/**
+ *
+ * @author helio
+ */
+public class SugarsyncJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
