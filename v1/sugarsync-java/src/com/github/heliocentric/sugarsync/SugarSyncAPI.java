931fe95b3d6b9ad57560911fea7c14a8337c76ac
Dylan Cochran <heliocentric@gmail.com>
2012-11-12 20:10:21 -0500

    * Add license

--- v1/sugarsync-java/src/com/github/heliocentric/sugarsync/SugarSyncAPI.java
+++ v1/sugarsync-java/src/com/github/heliocentric/sugarsync/SugarSyncAPI.java
@@ -1,6 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * Copyright (c) 2012, Dylan Cochran <heliocentric@gmail.com>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright 
+ * notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, 
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+ * PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * 
  */
 package com.github.heliocentric.sugarsync;
 
@@ -9,10 +23,14 @@ package com.github.heliocentric.sugarsync;
  * @author helio
  */
 public class SugarSyncAPI {
+	
+	private static final String AUTH_ACCESS_TOKEN_API_URL = "https://api.sugarsync.com/authorization";
 
 	public void start() {
+		
 	}
-
+	
+	
 	public SugarSyncAPI(String Username, String Password, String AppID, String KeyID, String PrivateKey) {
 		this.Username = Username;
 		this.Password = Password;
