b7931dd8f882ecfcf89b2399f785b87bacb60135
Dylan Cochran <heliocentric@gmail.com>
2012-11-27 21:30:50 -0500

    * Add Account information.

---
+++ v1/sugarsync-java/src/com/github/heliocentric/sugarsync/LocalStorage/Account.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.heliocentric.sugarsync.LocalStorage;
+
+/**
+ *
+ * @author Helio
+ */
+public class Account {
+	public StorageObject object;
+	public Account() {
+	}
+	public Account(StorageObject object) {
+		this.object = object;
+	}
+}
