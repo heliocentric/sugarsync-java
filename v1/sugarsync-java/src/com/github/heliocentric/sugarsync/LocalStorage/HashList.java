377e66da0b66b7a98092317efd20f6b725d373c1
helio <helio@helio-Satellite-L305>
2012-12-23 19:03:14 -0500

    * Add sha256/md5

--- v1/sugarsync-java/src/com/github/heliocentric/sugarsync/LocalStorage/HashList.java
+++ v1/sugarsync-java/src/com/github/heliocentric/sugarsync/LocalStorage/HashList.java
@@ -15,5 +15,17 @@ public class HashList {
 	public HashList(StorageObject object) {
 		this.object = object;
 	}
+	public String getSHA256() {
+		return this.object.getAttributeString("sha256");
+	}
+	public boolean setSHA256(String var) {
+		return this.object.setAttributeString("sha256", var);
+	}
 	
+	public String getMD5() {
+		return this.object.getAttributeString("md5");
+	}
+	public boolean setMD5(String var) {
+		return this.object.setAttributeString("md5", var);
+	}
 }
